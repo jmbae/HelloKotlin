07cd7cbbb1df88d0f363e6760c35281ca9d7d702
Jungman Bae <jmbae@liv.kr>
2018-11-05 17:43:47 +0900

    6강 함수와 함수형 프로그래밍

---
+++ src/class06/_02LocalFunctions.kt
@@ -0,0 +1,14 @@
+package class06
+
+fun foo(fooParam: String) {
+    val outerFunction = "Value"
+    fun bar(barParam: String) {
+        println(barParam)
+        println(fooParam)
+        println(outerFunction)
+    }
+}
+
+fun main(args: Array<String>) {
+    foo("Some value")
+}
---
+++ src/class06/_03Extensions.kt
@@ -0,0 +1,55 @@
+//@file:JvmName("StringUtil")
+package class06
+
+fun String.lastChar() = this.get(this.length - 1)
+
+// 'this' can be omitted
+fun String.lastChar1() = get(length - 1)
+
+fun test() {
+    // visible in completion
+    "abc".lastChar()
+}
+
+// Infix
+infix fun String.isEqual(value: String) = this == value
+
+/**
+ * Created by hadihariri on 24/08/16.
+ */
+
+fun String.hello() {
+    println("It's me!")
+}
+
+fun String.toTitleCase(prefix: String): String {
+    return this.split(" ").joinToString(" ") { "$prefix ${it.capitalize()}" }
+}
+
+fun main(args: Array<String>) {
+
+    val instance = InheritedClass()
+
+    instance.extension()
+}
+
+class Customer {
+    fun makePreferred() {
+        println("Customer version")
+    }
+}
+
+fun Customer.makePreferred(message: String) {
+    println(message)
+}
+
+open class BaseClass
+class InheritedClass: BaseClass()
+
+fun BaseClass.extension() {
+    println("Base extension")
+}
+
+fun InheritedClass.extension() {
+    println("Inherited extension")
+}
