1171068232e621368a16f2cc6da655cd4a93a9ed
jmbae <jungman82@gmail.com>
2018-11-20 19:24:24 +0900

    델리게이션 코드 추가

--- src/main/class10/_01Delegation.kt
+++ src/main/class10/_01Delegation.kt
@@ -1,6 +1,37 @@
 package main.class10
 
 import main.class07.Customer
+import kotlin.reflect.KProperty
+
+class Example {
+    var p: String by Delegate()
+}
+
+class Delegate {
+    operator fun getValue(thisRef: Any?, property: KProperty<*>): String {
+        return "$thisRef 에 할당된 Example 객체는 '${property.name}'를 Delegate 클래스에 위임했습니다."
+    }
+
+    operator fun setValue(thisRef: Any?, property: KProperty<*>, value: String) {
+        println("Example 클래스가 생성되어 ${thisRef}에 할당되고, $value 타입의 객체가 멤버변수 '${property.name}'에 할당되었습니다.")
+    }
+}
+
+fun main(args: Array<String>) {
+    val example = Example()
+    example.p = "String"
+    println(example.p)
+
+
+    //다른 어딘가에서
+    val repository = RepositoryImpl()
+    val logger = LoggerImpl()
+
+    val controller = Controller(repository, logger)
+
+    controller.getAll()
+    controller.logAll()
+}
 
 interface Repository {
     fun getById(id: Int): Customer
@@ -13,18 +44,6 @@ interface Logger {
 
 class Controller(repository: Repository, logger: Logger) : Repository by repository, Logger by logger {
 
-
-}
-
-fun main(args: Array<String>) {
-    //다른 어딘가에서
-    val repository = RepositoryImpl()
-    val logger = LoggerImpl()
-
-    val controller = Controller(repository, logger)
-
-    controller.getAll()
-    controller.logAll()
 }
 
 class RepositoryImpl: Repository {
@@ -47,6 +66,4 @@ class LoggerImpl: Logger {
     override fun logAll() {
         println("로그를 찍습니다.")
     }
-
 }
-
